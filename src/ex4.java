9852a202df880dcf172fdb2bcb853985b18423e4
Leticia Tambelli <leticiatambelli2@gmail.com>
2025-07-23 12:05:43 -0300

    exercicio 4

---
+++ src/ex4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ex4 {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite um número para saber sua tabuada: ");
+        int numero = leitura.nextInt();
+
+        for (int i = 0; i < 11; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+    }
+}
