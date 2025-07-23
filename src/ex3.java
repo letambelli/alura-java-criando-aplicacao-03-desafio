0fbfff45d83ee9897e307d5985d343a1ed532586
Leticia Tambelli <leticiatambelli2@gmail.com>
2025-07-23 11:51:05 -0300

    exercicio 3

---
+++ src/ex3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ex3 {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("1. Calcular área do quadrado");
+        System.out.println("2. Calcular área do círculo");
+        System.out.println("Digite sua opção: ");
+        int opcao = leitura.nextInt();
+
+        switch (opcao) {
+            case 1:
+                System.out.println("Qual o tamanho do lado do quadrado em metros?");
+                double ladoQuadrado = leitura.nextDouble();
+                double areaQuadrado = Math.pow(ladoQuadrado, 2);
+                System.out.println("Área do quadrado: " + areaQuadrado + "m²");
+            case 2:
+                System.out.println("Qual o raio do círculo em metros?");
+                double raioCirculo = leitura.nextDouble();
+                double areaCirculo = (3.14 * Math.pow(raioCirculo, 2));
+                System.out.println("Área do círculo: " + areaCirculo + "m²");
+        }
+
+    }
+}
